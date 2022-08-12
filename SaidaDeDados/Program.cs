00fd0e9864cabe90ca0527088c59cc49cd6ac7b7
Gmasterdemon <gleize.martin@gmail.com>
2022-08-12 16:23:39 -0300

    testando segundo commit

--- SaidaDeDados/Program.cs
+++ SaidaDeDados/Program.cs
@@ -14,8 +14,10 @@ Console.WriteLine("Produto:");
 Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
 Console.WriteLine($"{produto2}, cujo preco é $ {preco2:F2}");
 Console.WriteLine();
+
 Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero: {2}",idade,codigo,genero);
 Console.WriteLine();
+
 Console.WriteLine("Medida com oito casas decimais: {0:F8}",medida);
 Console.WriteLine("Arredondando (três casas decimais): {0:F3}",medida);
 Console.WriteLine("Separador decimal invariant culture: "+medida.ToString("F3",CultureInfo.InvariantCulture));
